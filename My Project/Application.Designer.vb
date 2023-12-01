1ac1869100f27079733b9dd77edc90dff2af88ed
enda adittia <223443009@mhs.polman-bandung.ac.id>
2023-12-01 13:09:59 +0700

    update

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.inventaris_barang.BarangInventaris
+            Me.MainForm = Global.inventaris_barang.Login
         End Sub
     End Class
 End Namespace
